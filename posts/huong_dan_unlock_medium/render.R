3db712ab6c2950be21f01591b93abfdbd8f2c35c
Duc Nguyen <46308688+tanduc307@users.noreply.github.com>
2026-01-17 02:30:45 +0700

    .

--- posts/huong_dan_unlock_medium/render.R
+++ posts/huong_dan_unlock_medium/render.R
@@ -1,3 +1,3 @@
-shell(cmd = 'npx staticrypt "docs/posts/huong-dan-unlock-medium/index.html" -p r -t template.html --short -d "docs/posts/huong-dan-unlock-medium"')
+shell(cmd = 'npx staticrypt "docs/posts/huong_dan_unlock_medium/index.html" -p r -t template.html --short -d "docs/posts/huong_dan_unlock_medium"')
 
 unlink(".staticrypt.json")
